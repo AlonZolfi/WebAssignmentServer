1ad300536253056449fefc460f9792c69eef2425
AlonZolfi <alonzolfi44@gmail.com>
2019-05-21 13:23:21 +0300

    started initial server and db connection

---
+++ Server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var DButilsAzure = require('./DButils');
+
+var port = 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
+
+app.get('/listCategories', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM Categories")
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err)
+        })
+});
