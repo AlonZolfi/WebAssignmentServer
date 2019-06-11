4f23dcdb65379348cf5f5f1c4dff913c38666624
hilakhadad <hilakhadad@gmail.com>
2019-06-11 12:40:46 +0300

    logIn is good to go

--- Route/pages/logIn/logInController.js
+++ Route/pages/logIn/logInController.js
@@ -1,5 +1,5 @@
 angular.module("myApp")
-    .controller("logInController", function ($scope,$http) {
+    .controller("logInController", function ($scope,$http,$location) {
         $scope.username = "Guest";
         $scope.submit = function() {
             var myObj = {username: $scope.user_name.valueOf(),password: $scope.user_pass.valueOf()};
@@ -7,10 +7,12 @@ angular.module("myApp")
             $http.post('http://localhost:3000/login', myJSON)
                 .then(function (response) {
                     $scope.username = $scope.user_name.valueOf();
-
+                    $scope.userToken = response.data;
+                    alert("You are sing in.. just need to know were to move you")
+                    $location.path("/"); //need to be a "recommended for you"
                 })
                 .catch(function (error) {
-                    console.log("onononon");
+                    alert("Wrong user mane or password, \nplease try again")
                 });
         };
     });
